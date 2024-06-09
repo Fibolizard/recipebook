79701764d2064ca639ab391ed106b1d0ab86bfb1
Juan <juan_urrea82231@elpoli.edu.co>
2024-06-09 13:11:00 -0500

    Tercer commit

--- scripts/login.js
+++ scripts/login.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
     $('#loginButton').click(function(){
         var username = $('#username').val();
         var password = $('#password').val();
+
         if(username === "root" && password === "12345"){
             
-            window.location.href = "index.html"; // Cambia "main.html" por la URL de la página principal
+            window.location.href = "index.html"; 
         } else {
             alert("Usuario o contraseña incorrectos");
         }
+    });
+
+    $('#login_landing').click(function(){
+
+        window.location.href = "login.html"; 
+
+
+
     });
 });
